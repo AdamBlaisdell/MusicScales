0b086da2756c16ed084f26389b646c5667dc1080
AdamPC <adam.s.blaisdell@gmail.com>
2024-08-09 13:40:06 -0400

    Add project files.

---
+++ MusicScales/MusicScales.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicScales
+{
+    internal class MusicScales
+    {
+        // a list of musical notes and enharmonic equivalents
+        public static string[][] Notes = new string[][]
+        {
+            new string[] { "B#",  "C",  "Dbb" },
+            new string[] { "B##", "C#", "Db"  },
+            new string[] { "C##", "D",  "Ebb" },
+            new string[] { "D#",  "Eb", "Fbb" },
+            new string[] { "D##", "E",  "Fb"  },
+            new string[] { "E#",  "F",  "Gbb" },
+            new string[] { "E##", "F#", "Gb"  },
+            new string[] { "F##", "G",  "Abb" },
+            new string[] { "G#",  "Ab"        },
+            new string[] { "G##", "A",  "Bbb" },
+            new string[] { "A#",  "Bb", "Cbb" },
+            new string[] { "A##", "B",  "Cb"  }
+        };
+    }
+}
